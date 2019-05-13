c6d09fab899b05a28b44f5e9dcf15462a4a6edc2
cuteLittleMango <shenmin881101@163.com>
2019-05-13 10:33:14 +0800

    springcloud config学习

---
+++ springcloud/config-server/src/main/java/com/shenmin/configServer/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.shenmin.configServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 描述〈configServer启动类〉
+ *
+ * @author shen.min
+ * @create 2019/5/13 10:16
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
